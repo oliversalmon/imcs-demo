d67be07b3869564c8e23817301b359bd7e930f98
Oliver Buckley-Salmon <oliver.salmon@gmail.com>
2017-05-02 20:50:23 +0100

    cleanup

--- domain/src/main/java/com/example/mu/domain/Party.java
+++ domain/src/main/java/com/example/mu/domain/Party.java
@@ -1,7 +1,113 @@
 package com.example.mu.domain;
 
+import com.google.gson.Gson;
+import com.hazelcast.nio.serialization.Portable;
+import com.hazelcast.nio.serialization.PortableReader;
+import com.hazelcast.nio.serialization.PortableWriter;
+
+import java.io.IOException;
+
 /**
  * Created by oliverbuckley-salmon on 28/04/2017.
  */
-public class Party {
+public class Party implements Portable{
+
+    private String partyId
+    ,               shortName
+    ,               name
+    ,               role;
+    public static final int FACTORY_ID = 0
+    ,                       CLASS_ID = 3;
+
+    public Party() {
+    }
+
+    public Party(String partyId, String shortName, String name, String role) {
+        this.partyId = partyId;
+        this.shortName = shortName;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getPartyId() {
+        return partyId;
+    }
+
+    public void setPartyId(String partyId) {
+        this.partyId = partyId;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getFactoryId() {
+        return FACTORY_ID;
+    }
+
+    public int getClassId() {
+        return CLASS_ID;
+    }
+
+    public void writePortable(PortableWriter out) throws IOException {
+        out.writeUTF("partyId", partyId);
+        out.writeUTF("shortName", shortName);
+        out.writeUTF("name", name);
+        out.writeUTF("role", role);
+    }
+
+
+    public void readPortable(PortableReader in) throws IOException {
+        this.partyId = in.readUTF("partyId");
+        this.shortName = in.readUTF("shortName");
+        this.name = in.readUTF("name");
+        this.role = in.readUTF("role");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Party party = (Party) o;
+
+        if (!partyId.equals(party.partyId)) return false;
+        if (!shortName.equals(party.shortName)) return false;
+        if (name != null ? !name.equals(party.name) : party.name != null) return false;
+        return role != null ? role.equals(party.role) : party.role == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = partyId.hashCode();
+        result = 31 * result + shortName.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        return result;
+    }
+
+    public String toJSON(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
 }
--- domain/src/main/java/com/example/mu/domain/Price.java
+++ domain/src/main/java/com/example/mu/domain/Price.java
@@ -1,7 +1,117 @@
 package com.example.mu.domain;
 
+import com.google.gson.Gson;
+import com.hazelcast.nio.serialization.Portable;
+import com.hazelcast.nio.serialization.PortableReader;
+import com.hazelcast.nio.serialization.PortableWriter;
+
+import java.io.IOException;
+
 /**
  * Created by oliverbuckley-salmon on 28/04/2017.
  */
-public class Price {
+public class Price implements Portable{
+
+    private String  priceId
+    ,               instrumentId;
+    private double  price;
+    private long    timeStamp;
+    public static final int FACTORY_ID = 0
+    ,                       CLASS_ID = 4;
+
+
+    public Price() {
+    }
+
+    public Price(String priceId, String instrumentId, double price, long timeStamp) {
+        this.priceId = priceId;
+        this.instrumentId = instrumentId;
+        this.price = price;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getPriceId() {
+        return priceId;
+    }
+
+    public void setPriceId(String priceId) {
+        this.priceId = priceId;
+    }
+
+    public String getInstrumentId() {
+        return instrumentId;
+    }
+
+    public void setInstrumentId(String instrumentId) {
+        this.instrumentId = instrumentId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getFactoryId() {
+        return FACTORY_ID;
+    }
+
+    public int getClassId() {
+        return CLASS_ID;
+    }
+
+    public void writePortable(PortableWriter out) throws IOException {
+        out.writeUTF("priceId", priceId);
+        out.writeUTF("instrumentId", instrumentId);
+        out.writeDouble("price", price);
+        out.writeLong("timeStamp", timeStamp);
+    }
+
+
+    public void readPortable(PortableReader in) throws IOException {
+        this.priceId = in.readUTF("priceId");
+        this.instrumentId = in.readUTF("instrumentId");
+        this.price = in.readDouble("price");
+        this.timeStamp = in.readLong("timeStamp");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Price price1 = (Price) o;
+
+        if (Double.compare(price1.price, price) != 0) return false;
+        if (timeStamp != price1.timeStamp) return false;
+        if (!priceId.equals(price1.priceId)) return false;
+        return instrumentId.equals(price1.instrumentId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = priceId.hashCode();
+        result = 31 * result + instrumentId.hashCode();
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (int) (timeStamp ^ (timeStamp >>> 32));
+        return result;
+    }
+
+    public String toJSON(){
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
 }
