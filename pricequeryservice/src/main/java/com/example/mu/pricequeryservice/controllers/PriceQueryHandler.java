384928d009b4c47dd50c1d8b2c2573677a4a7b8c
dineshgpillai <dinesh.g.pillai@gmail.com>
2017-10-16 17:09:48 +0100

    Further work on Reactive

--- pricequeryservice/src/main/java/com/example/mu/pricequeryservice/controllers/PriceQueryHandler.java
+++ pricequeryservice/src/main/java/com/example/mu/pricequeryservice/controllers/PriceQueryHandler.java
@@ -2,22 +2,35 @@ package com.example.mu.pricequeryservice.controllers;
 
 import com.example.mu.domain.Price;
 import com.example.mu.pricequeryservice.repository.PriceRepository;
+import com.hazelcast.core.IMap;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
 
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 
-@Service
+
+@RestController
 public class PriceQueryHandler {
 
+	final Logger LOG = LoggerFactory.getLogger(PriceQueryHandler.class);
 	@Autowired
 	private PriceRepository priceRepo;
 
@@ -31,6 +44,19 @@ public class PriceQueryHandler {
 		return priceRepo.getPrice(request.pathVariable("id"))
 				.flatMap(price -> ServerResponse.ok().contentType(APPLICATION_JSON).body(Mono.just(price), Price.class))
 				.switchIfEmpty(ServerResponse.notFound().build());
+	}
+	
+	@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE, value="pricequeryservice/pricestream")
+	public Flux<Price> getFluxPrices(){
+		
+		LOG.info("In pricestream...");
+	
+		Flux<Price> prices =  priceRepo.getAllPrices();
+		Flux<Long> duration = Flux.interval(Duration.ofSeconds(1));
+		
+		LOG.info("returning flux prices...");
+			return Flux.zip(prices, duration).map(Tuple2::getT1);
+		
 	}
 
 	public static void main(String[] args) {
--- pricequeryservice/src/main/java/com/example/mu/pricequeryservice/repository/PriceRepository.java
+++ pricequeryservice/src/main/java/com/example/mu/pricequeryservice/repository/PriceRepository.java
@@ -2,11 +2,17 @@ package com.example.mu.pricequeryservice.repository;
 
 import static com.hazelcast.query.Predicates.equal;
 
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.mu.domain.Price;
+import com.example.mu.pricequeryservice.controllers.PriceQueryHandler;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
@@ -14,10 +20,13 @@ import com.hazelcast.query.Predicate;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 @Repository
+
 public class PriceRepository {
 
+	final Logger LOG = LoggerFactory.getLogger(PriceRepository.class);
 	private final static String PRICE_MAP = "price";
 
 	@Bean
@@ -39,6 +48,17 @@ public class PriceRepository {
 
 	}
 
+	public Flux<Price> getIntermittentPrices() {
+
+		LOG.info("In intermittent prices...");
+		IMap<String, Price> priceMap = hazelcastInstance.getMap(PRICE_MAP);
+
+		Flux<Price> prices =  Flux.fromStream(priceMap.values().stream());
+		Flux<Long> duration = Flux.interval(Duration.ofSeconds(1));
+			return Flux.zip(prices, duration).map(Tuple2::getT1);
+
+	}
+	
 	public Mono<Price> getPrice(String instrumentId) {
 		IMap<String, Price> priceMap = hazelcastInstance.getMap(PRICE_MAP);
 		Predicate priceFilter = equal("instrumentId", instrumentId);
