4e4d2fa27740c6bcb9b3cc30fbdb821cdf4e10d1
dineshgpillai <dinesh.g.pillai@gmail.com>
2017-12-08 11:33:04 +0000

    First cut of flink

---
+++ tradeInjectorFlink/src/main/java/com/mu/flink/streamer/TradeFlinkStreamer.java
@@ -0,0 +1,32 @@
+package com.mu.flink.streamer;
+
+import org.apache.zookeeper.version.Info;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+
+
+
+public class TradeFlinkStreamer {
+	
+	final static Logger LOG = LoggerFactory.getLogger(TradeFlinkStreamer.class);
+	
+	
+	public static void main(String[] args) throws Exception {
+		
+		ClassLoader classLoader = TradeFlinkStreamer.class.getClassLoader();
+		LOG.info("Starting Trade Flink Streamer");
+		System.out.println("Zookeeper version used is "+Info.MAJOR+"."+Info.MINOR+"."+Info.MICRO);
+	    //
+		
+		Class aClass = classLoader.loadClass("com.hazelcast.zookeeper.ZookeeperDiscoveryStrategy");
+        System.out.println("aClass.getName() = " + aClass.getName());
+        HazelcastInstance hzClient = HazelcastClient.newHazelcastClient();
+		LOG.info("Completed Trade Flink Streamer");
+		//hzClient.shutdown();
+		
+	}
+
+}
