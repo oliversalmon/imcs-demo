84bead5313cb5932956ba665e29f30aa98d7e56c
dineshgpillai <dinesh.g.pillai@gmail.com>
2017-12-13 15:28:48 +0100

    Now connects to Kafka

--- tradeInjectorFlink/src/main/java/com/mu/flink/streamer/TradeFlinkStreamer.java
+++ tradeInjectorFlink/src/main/java/com/mu/flink/streamer/TradeFlinkStreamer.java
@@ -1,9 +1,22 @@
 package com.mu.flink.streamer;
 
-import org.apache.zookeeper.version.Info;
+import java.util.Properties;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.example.mu.domain.Trade;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 
@@ -12,18 +25,78 @@ import com.hazelcast.core.HazelcastInstance;
 public class TradeFlinkStreamer {
 	
 	final static Logger LOG = LoggerFactory.getLogger(TradeFlinkStreamer.class);
+	 HazelcastInstance hzClient = HazelcastClient.newHazelcastClient();
+	 private static Gson gson = new GsonBuilder().create();
+	 
+	 public Properties consumerConfigs() {
+			Properties props = new Properties();
+			// list of host:port pairs used for establishing the initial connections
+			// to the Kakfa cluster
+			props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+					"178.62.124.180:9092");
+			props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+					StringDeserializer.class);
+			props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+					JsonDeserializer.class);
+			props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+			
+			// allows a pool of processes to divide the work of consuming and
+			// processing records
+			props.put(ConsumerConfig.GROUP_ID_CONFIG, "mu");
+
+			return props;
+		}
+	 
+	 public void connectToTradeStream() throws Exception {
+			StreamExecutionEnvironment env = StreamExecutionEnvironment
+					.getExecutionEnvironment();
+			
+			FlinkKafkaConsumer010 kafkaConsumer = new FlinkKafkaConsumer010(
+					"trade", new SimpleStringSchema(), consumerConfigs());
+			DataStream<String> stream = env.addSource(kafkaConsumer);
+			
+			stream.map(new MapFunction<String, Trade>() {
+				private static final long serialVersionUID = -6867736771747690202L;
+				
+				
+				//returns a price object
+				
+				public Trade map(String value) throws Exception {
+					
+						
+			            Trade p = gson.fromJson(value, Trade.class);
+
+					return p;
+				}
+			}).map(new MapFunction<Trade,String>(){
+
+				//stores the price object in hazelcast
+			
+				public String map(Trade arg0) throws Exception {
+					
+					
+					//IMap<String, String> priceMap = hazelcastInstance.getMap("price");
+					//priceMap.put(arg0.getInstrumentId(), arg0);
+					return "Trade stored in hz "+arg0.getInstrumentId();
+					
+				}
+				
+			}).print();
+			
+			env.execute();
+	 }
 	
 	
 	public static void main(String[] args) throws Exception {
 		
 		ClassLoader classLoader = TradeFlinkStreamer.class.getClassLoader();
 		LOG.info("Starting Trade Flink Streamer");
-		System.out.println("Zookeeper version used is "+Info.MAJOR+"."+Info.MINOR+"."+Info.MICRO);
+		//System.out.println("Zookeeper version used is "+Info.MAJOR+"."+Info.MINOR+"."+Info.MICRO);
 	    //
-		
-		Class aClass = classLoader.loadClass("com.hazelcast.zookeeper.ZookeeperDiscoveryStrategy");
+		new TradeFlinkStreamer().connectToTradeStream();
+		//Class aClass = classLoader.loadClass("com.hazelcast.zookeeper.ZookeeperDiscoveryStrategy");
         //System.out.println("aClass.getName() = " + aClass.getName());
-        HazelcastInstance hzClient = HazelcastClient.newHazelcastClient();
+       
 		LOG.info("Completed Trade Flink Streamer");
 		//hzClient.shutdown();
 		
