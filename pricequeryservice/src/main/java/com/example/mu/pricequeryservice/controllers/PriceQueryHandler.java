13baf4ad0b1d7cf576a8cb96f335c20da9adfd27
dineshgpillai <dinesh.g.pillai@gmail.com>
2018-01-22 16:42:44 +0000

    Further updates to Price Query Handler to register with Zoo

--- pricequeryservice/src/main/java/com/example/mu/pricequeryservice/controllers/PriceQueryHandler.java
+++ pricequeryservice/src/main/java/com/example/mu/pricequeryservice/controllers/PriceQueryHandler.java
@@ -17,23 +17,89 @@ import java.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.serviceregistry.Registration;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @RestController
 @EnableWebFlux
+@EnableFeignClients
 public class PriceQueryHandler {
+	
+	@Value("${spring.application.name:priceQueryService}")
+	private String appName;
 
 	final Logger LOG = LoggerFactory.getLogger(PriceQueryHandler.class);
 	@Autowired
 	private PriceRepository priceRepo;
+	
+	@Autowired
+	private LoadBalancerClient loadBalancer;
+
+	@Autowired
+	private DiscoveryClient discovery;
+
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	private AppClient appClient;
+
+	@Autowired(required = false)
+	private Registration registration;
+	
+	@Autowired
+	RestTemplate rest;
+	
+	@RequestMapping("/hi")
+	public String hi() {
+		return "Hello World! from " + this.registration;
+	}
+	
+
+	@RequestMapping("/")
+	public ServiceInstance lb() {
+		return this.loadBalancer.choose(this.appName);
+	}
+	
+	@RequestMapping("/myenv")
+	public String env(@RequestParam("prop") String prop) {
+		return this.env.getProperty(prop, "Not Found");
+	}
+	
+	@RequestMapping("/self")
+	public String self() {
+		return this.appClient.hi();
+	}
+	
+	@FeignClient("priceQueryService")
+	interface AppClient {
+		@RequestMapping(path = "pricequeryservice/pricestream", method = RequestMethod.GET)
+		Flux<Price> getFluxPrices();
+		@RequestMapping(path = "/ping", method = RequestMethod.GET)
+		String hi();
+		
+	}
 
 	public Mono<ServerResponse> getAllPrices(ServerRequest request) {
 
@@ -59,7 +125,17 @@ public class PriceQueryHandler {
 		return prices;
 
 	}
+	
+	@Bean
+	@LoadBalanced
+	RestTemplate loadBalancedRestTemplate() {
+		return new RestTemplate();
+	}
 
+	public String rt() {
+		return this.rest.getForObject("http://" + this.appName + "/hi", String.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PriceQueryHandler.class, args);
 
---
+++ pricequeryservice/src/test/java/com/test/pricequeryservice/PriceQueryTest.java
@@ -0,0 +1,40 @@
+package com.test.pricequeryservice;
+
+import static org.junit.Assert.*;
+
+import org.apache.curator.test.TestingServer;
+import org.junit.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.SocketUtils;
+
+import com.example.mu.domain.Price;
+import com.example.mu.pricequeryservice.controllers.StartUp;
+
+import reactor.core.publisher.Flux;
+
+public class PriceQueryTest {
+	
+
+	@Test public void contextLoads() throws Exception {
+		int zkPort = SocketUtils.findAvailableTcpPort();
+		TestingServer server = new TestingServer(zkPort);
+
+		int port = SocketUtils.findAvailableTcpPort(zkPort+1);
+
+		ConfigurableApplicationContext context = new SpringApplicationBuilder(StartUp.class).run(
+				"--server.port="+port,
+				"--management.endpoints.web.expose=*",
+				"--spring.cloud.zookeeper.connect-string=localhost:" + zkPort);
+
+		ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://priceQueryService/hi", String.class);
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+
+		context.close();
+		server.close();
+	}
+
+}
