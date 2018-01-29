8866d216336572931a4be5eebf860a4172e345e1
Dinesh Pillai <dinesh.g.pillai@gmail.com>
2018-01-29 11:15:53 +0000

    dynamically sets the IP and host for Position query service

---
+++ kubernetes/setKubeIP.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+#first extract the postion query services  
+POSITION_QUERY_SERVICE=$(kubectl get pods -n=mu-architecture-demo | grep position |  awk '{print $1}')
+cp ~/mu-architecture-core/kubernetes/file.txt ~/mu-architecture-core/kubernetes/file2.txt
+cp ~/mu-architecture-core/kubernetes/run-apps-reports.yaml ~/mu-architecture-core/kubernetes/run-apps-reports-dep.yaml
+
+counter=1
+for i in $POSITION_QUERY_SERVICE
+do
+  export "IP=$(kubectl get pod ${i} --template={{.status.podIP}} -n=mu-architecture-demo)"
+  sh -c  "echo \$IP"
+  sed -i "s/IP_${counter}/${IP}/g" ~/mu-architecture-core/kubernetes/run-apps-reports-dep.yaml 
+  sed -i "s/HOST_${counter}/${i}/g" ~/mu-architecture-core/kubernetes/run-apps-reports-dep.yaml
+  counter=$((counter+1))
+done
+
+counter=1
+for i in $POSITION_QUERY_SERVICE
+do
+  export "POSITION_SERVICE_HOST_${counter}=${i}"
+  sh -c  "echo \$POSITION_SERVICE_HOST_${counter}"
+  counter=$((counter+1))
+done
