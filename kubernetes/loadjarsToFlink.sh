39eefa462bb38f3bb0a2d2bf0399b6e575500ceb
pillaid <dinesh.pillai@ig.com>
2018-11-16 13:36:34 +0000

    Further modification to flink streamers to pick up zookeeper

--- kubernetes/loadjarsToFlink.sh
+++ kubernetes/loadjarsToFlink.sh
@@ -4,7 +4,7 @@ cd ~/imcs-demo/tradeInjectorFlink/target
 
 JOB_MANAGER_NAME=$(kubectl get pods -n=mu-architecture-demo | grep jobmanager |  awk '{print $1}')
 echo $JOB_MANAGER_NAME
-kubectl cp mu-flink-trade-injector-0.0.1-SNAPSHOT.jar mu-architecture-demo/$JOB_MANAGER_NAME:/tmp/dummy.jar
+kubectl cp mu-flink-trade-injector-0.0.1-SNAPSHOT.jar mu-architecture-demo/$JOB_MANAGER_NAME:/tmp/mu-flink-trade-injector-0.0.1-SNAPSHOT.jar
 
 #once copy is successful load the jar within the container
 nohup kubectl exec -it $JOB_MANAGER_NAME -n=mu-architecture-demo ./bin/flink run /tmp/mu-flink-trade-injector-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
@@ -12,5 +12,5 @@ nohup kubectl exec -it $JOB_MANAGER_NAME -n=mu-architecture-demo ./bin/flink run
 
 cd ~/imcs-demo/priceInjectorFlink/target
 
-kubectl cp mu-flink-price-injector-0.0.1-SNAPSHOT.jar mu-architecture-demo/$JOB_MANAGER_NAME:/tmp/dummy.jar
+kubectl cp mu-flink-price-injector-0.0.1-SNAPSHOT.jar mu-architecture-demo/$JOB_MANAGER_NAME:/tmp/mu-flink-price-injector-0.0.1-SNAPSHOT.jar
 nohup kubectl exec -it $JOB_MANAGER_NAME -n=mu-architecture-demo ./bin/flink run  /tmp/mu-flink-price-injector-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
--- trade-injector/src/main/java/com/trade/injector/sinks/KafkaSink.java
+++ trade-injector/src/main/java/com/trade/injector/sinks/KafkaSink.java
@@ -33,8 +33,8 @@ public class KafkaSink implements ISink {
 
 			@Override
 			public void onSuccess(SendResult<String, String> result) {
-				LOG.info("sent message='{}' with offset={}", message, result
-						.getRecordMetadata().offset());
+				//LOG.info("sent message='{}' with offset={}", message, result
+						//.getRecordMetadata().offset());
 			}
 
 			@Override
