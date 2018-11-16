c397356ce4112145e1d8c218d3dc7ae6bdf4ec9a
pillaid <dinesh.pillai@ig.com>
2018-11-16 15:02:09 +0000

    Fixed error on Price scheduler

--- trade-injector/src/main/java/com/trade/injector/schedulers/PriceScheduler.java
+++ trade-injector/src/main/java/com/trade/injector/schedulers/PriceScheduler.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
