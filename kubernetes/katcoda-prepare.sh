28f21fc64e050e5b8c6ca41341916c69c8d7794d
pillaid <dinesh.pillai@ig.com>
2018-12-04 17:35:19 +0000

    Further changes to the katcoda script

--- kubernetes/katcoda-prepare.sh
+++ kubernetes/katcoda-prepare.sh
@@ -1,11 +1,12 @@
 #Install Java and Maven
+apt install software-properties-common -y
 add-apt-repository ppa:webupd8team/java
 apt-get update -y
 apt-get install oracle-java8-installer -y
 
 apt install maven -y
 
-cd imcs-demo
+cd ../
 docker login -u dineshpillai -p Pill2017
 
 mvn clean package install -DskipTests
@@ -14,3 +15,7 @@ cd ./pricequeryservice
 
 mvn docker:build
 docker push dineshpillai/mu-pricequery-service
+
+
+kubectl apply -f price-query-configmap.yml
+kubectl apply -f price-service.yaml
