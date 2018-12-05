61dfa9aeeb7a2b047e062a3532092328660a0347
pillaid <dinesh.pillai@ig.com>
2018-12-05 11:08:47 +0000

    Further changes to Price service

--- pricequeryservice/src/main/java/com/example/mu/pricequeryservice/controllers/StartUp.java
+++ pricequeryservice/src/main/java/com/example/mu/pricequeryservice/controllers/StartUp.java
@@ -2,7 +2,7 @@ package com.example.mu.pricequeryservice.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.example.mu.pricequeryservice")
--- pricequeryservice/src/main/java/com/example/mu/pricequeryservice/repository/PriceRepository.java
+++ pricequeryservice/src/main/java/com/example/mu/pricequeryservice/repository/PriceRepository.java
@@ -1,7 +1,7 @@
 package com.example.mu.pricequeryservice.repository;
 
 import com.example.mu.domain.Price;
-import com.hazelcast.client.HazelcastClient;
+//import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
 import com.hazelcast.query.Predicate;
@@ -30,16 +30,16 @@ public class PriceRepository {
 	private static ConcurrentLinkedQueue<Price> listOfPrices = new ConcurrentLinkedQueue<Price>();
 
 
-	@Bean
-	// @Profile("client")
-	HazelcastInstance hazelcastInstance() {
-
-		return HazelcastClient.newHazelcastClient();
-
-	}
-
-	@Autowired
-	private HazelcastInstance hazelcastInstance;
+//	@Bean
+//	// @Profile("client")
+//	HazelcastInstance hazelcastInstance() {
+//
+//		return HazelcastClient.newHazelcastClient();
+//
+//	}
+//
+//	@Autowired
+//	private HazelcastInstance hazelcastInstance;
 
 //	public Flux<Price> getAllPrices() {
 //
@@ -56,33 +56,33 @@ public class PriceRepository {
 //
 //	}
 
-	public List<Price> getAllPxs(){
-		IMap<String, Price> priceMap = hazelcastInstance.getMap(PRICE_MAP);
-		List<Price> listOfPxs = new ArrayList<Price>();
+//	public List<Price> getAllPxs(){
+//		IMap<String, Price> priceMap = hazelcastInstance.getMap(PRICE_MAP);
+//		List<Price> listOfPxs = new ArrayList<Price>();
+//
+//		if (listOfPrices.size() < 1)
+//			priceMap.values().stream().forEach(a -> listOfPxs.add(a));
+//
+//		return listOfPxs;
+//	}
 
-		if (listOfPrices.size() < 1)
-			priceMap.values().stream().forEach(a -> listOfPxs.add(a));
-
-		return listOfPxs;
-	}
-
-	@Scheduled(fixedDelay = 100000)
-	private void updatePrices() {
-
-		LOG.info("Updating Price Stream...; current price count is " + listOfPrices.size());
-
-		IMap<String, Price> priceMap = hazelcastInstance.getMap(PRICE_MAP);
-		priceMap.values().stream().sorted((Price p1, Price p2) -> p1.getInstrumentId().compareTo(p2.getInstrumentId()))
-				.forEach(listOfPrices::add);
-
-		if (listOfPrices.size() > priceMap.size() * 2) {
-			//remove the first badge
-			for (int i = 0; i < priceMap.size(); i++)
-				listOfPrices.remove();
-		}
-
-		LOG.info("Done");
-	}
+//	@Scheduled(fixedDelay = 100000)
+//	private void updatePrices() {
+//
+//		LOG.info("Updating Price Stream...; current price count is " + listOfPrices.size());
+//
+//		IMap<String, Price> priceMap = hazelcastInstance.getMap(PRICE_MAP);
+//		priceMap.values().stream().sorted((Price p1, Price p2) -> p1.getInstrumentId().compareTo(p2.getInstrumentId()))
+//				.forEach(listOfPrices::add);
+//
+//		if (listOfPrices.size() > priceMap.size() * 2) {
+//			//remove the first badge
+//			for (int i = 0; i < priceMap.size(); i++)
+//				listOfPrices.remove();
+//		}
+//
+//		LOG.info("Done");
+//	}
 
 //	public Mono<Price> getPrice(String instrumentId) {
 //		IMap<String, Price> priceMap = hazelcastInstance.getMap(PRICE_MAP);
