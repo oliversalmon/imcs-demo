3a3dc46835b290bd532735a3ab2804e905c70966
pillaid <dinesh.pillai@ig.com>
2018-12-05 10:45:01 +0000

    Providing a docker file

--- kubernetes/katcoda-prepare.sh
+++ kubernetes/katcoda-prepare.sh
@@ -1,6 +1,6 @@
 #Install Java and Maven
 apt install software-properties-common -y
-add-apt-repository ppa:webupd8team/java
+add-apt-repository ppa:webupd8team/java -y
 apt-get update -y
 apt-get install oracle-java8-installer -y
 
@@ -11,11 +11,13 @@ docker login -u dineshpillai -p Pill2017
 
 mvn clean package install -DskipTests
 
+
 cd ./pricequeryservice
 
-mvn docker:build
+docker build -t dineshpillai/mu-pricequery-service .
 docker push dineshpillai/mu-pricequery-service
 
+kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=default:default
 
 kubectl apply -f price-query-configmap.yml
 kubectl apply -f price-service.yaml
