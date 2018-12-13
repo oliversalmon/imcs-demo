0e8299a8061d9939beffa4eaca884f701008c367
pillaid <dinesh.pillai@ig.com>
2018-12-13 17:17:32 +0000

    Switched on Hz

--- positionqueryservice/src/main/java/com/example/mu/positionqueryservice/PositionQueryService.java
+++ positionqueryservice/src/main/java/com/example/mu/positionqueryservice/PositionQueryService.java
@@ -1,13 +1,28 @@
 package com.example.mu.positionqueryservice;
 
+import com.example.mu.domain.PositionAccount;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.query.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.hazelcast.query.Predicates.and;
+import static com.hazelcast.query.Predicates.equal;
 
 
 @EnableCaching
@@ -20,57 +35,57 @@ public class PositionQueryService {
 
 
 
-//    @Bean
-//    HazelcastInstance hazelcastInstance() {
-//
-//        return HazelcastClient.newHazelcastClient();
-//
-//    }
-//
-////    //only will be used for testing
-//    @Value("${requireHz}")
-//    private String requireHz;
-//
-//
-//
-//    @Autowired
-//    private HazelcastInstance hazelcastInstance;
-//
-//
-//    @RequestMapping(value = "/getAllPositionAccounts", method = RequestMethod.GET)
-//    public ResponseEntity<List<PositionAccount>> getAllPositionAccounts() {
-//
-//        IMap<String, PositionAccount> posMap = hazelcastInstance.getMap(POSITION_ACCOUNT_MAP);
-//        posMap.size();
-//        //do not load on test
-//        if (requireHz.equals("false") )
-//            posMap.loadAll(true);
-//        return ResponseEntity.ok(posMap.values().stream()
-//                //.map(a -> a.toJSON())
-//                .collect(Collectors.toList()));
-//
-//    }
-//
-//    @RequestMapping(value = "/getPositionAccount/{positionAccountId}", method = RequestMethod.GET)
-//    public ResponseEntity<List<PositionAccount>> getPositionAccount(@PathVariable String positionAccountId) {
-//
-//        IMap<String, PositionAccount> posMap = hazelcastInstance.getMap(POSITION_ACCOUNT_MAP);
-//        Predicate positionAccount = equal("accountId", positionAccountId);
-//        return ResponseEntity.ok(posMap.values(positionAccount).stream().collect(Collectors.toList()));
-//
-//    }
-//
-//    @RequestMapping(value = "/getPositionAccountAndInstrument/{positionAccountId}/{instrumentId}", method = RequestMethod.GET)
-//    public ResponseEntity<List<Object>> getPositionAccountForInstrument(@PathVariable String positionAccountId,
-//                                                                        @PathVariable String instrumentId) {
-//
-//        IMap<String, PositionAccount> posMap = hazelcastInstance.getMap(POSITION_ACCOUNT_MAP);
-//        Predicate positionAccount = equal("accountId", positionAccountId);
-//        Predicate instrumentPredicate = equal("instrumentid", instrumentId);
-//        Predicate predicate = and(positionAccount, instrumentPredicate);
-//        return ResponseEntity.ok(posMap.values(predicate).stream().collect(Collectors.toList()));
-//
-//    }
+    @Bean
+    HazelcastInstance hazelcastInstance() {
+
+        return HazelcastClient.newHazelcastClient();
+
+    }
+
+//    //only will be used for testing
+    @Value("${requireHz}")
+    private String requireHz;
+
+
+
+    @Autowired
+    private HazelcastInstance hazelcastInstance;
+
+
+    @RequestMapping(value = "/getAllPositionAccounts", method = RequestMethod.GET)
+    public ResponseEntity<List<PositionAccount>> getAllPositionAccounts() {
+
+        IMap<String, PositionAccount> posMap = hazelcastInstance.getMap(POSITION_ACCOUNT_MAP);
+        posMap.size();
+        //do not load on test
+        if (requireHz.equals("false") )
+            posMap.loadAll(true);
+        return ResponseEntity.ok(posMap.values().stream()
+                //.map(a -> a.toJSON())
+                .collect(Collectors.toList()));
+
+    }
+
+    @RequestMapping(value = "/getPositionAccount/{positionAccountId}", method = RequestMethod.GET)
+    public ResponseEntity<List<PositionAccount>> getPositionAccount(@PathVariable String positionAccountId) {
+
+        IMap<String, PositionAccount> posMap = hazelcastInstance.getMap(POSITION_ACCOUNT_MAP);
+        Predicate positionAccount = equal("accountId", positionAccountId);
+        return ResponseEntity.ok(posMap.values(positionAccount).stream().collect(Collectors.toList()));
+
+    }
+
+    @RequestMapping(value = "/getPositionAccountAndInstrument/{positionAccountId}/{instrumentId}", method = RequestMethod.GET)
+    public ResponseEntity<List<Object>> getPositionAccountForInstrument(@PathVariable String positionAccountId,
+                                                                        @PathVariable String instrumentId) {
+
+        IMap<String, PositionAccount> posMap = hazelcastInstance.getMap(POSITION_ACCOUNT_MAP);
+        Predicate positionAccount = equal("accountId", positionAccountId);
+        Predicate instrumentPredicate = equal("instrumentid", instrumentId);
+        Predicate predicate = and(positionAccount, instrumentPredicate);
+        return ResponseEntity.ok(posMap.values(predicate).stream().collect(Collectors.toList()));
+
+    }
 
     @RequestMapping(value = "/hi_getall")
     public List<String> getall() {
