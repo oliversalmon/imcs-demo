6ae061348af8f85ec7f868ef46d32b8aa597137e
pillaid <dinesh.pillai@ig.com>
2019-01-09 10:57:30 +0000

    MU-114 Added the Kubernetes configs

---
+++ hbase-hdfs/katcoda-prepare-hbase.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#Create the docker images
+docker login -u dineshpillai -p Pill2017
+
+cd ~/imcs-demo/hbase-hdfs/hadoop
+make
+
+cd ~/imcs-demo/hbase-hdfs/hbase
+make
+
+#Apply the kube configs
+kubectl create namespace mu-architecture-demo
+kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=mu-architecture-demo:default
+
+cd ~/imcs-demo/kubernetes
+kubectl apply -f run-mzk.yaml
+
+
+cd ~/imcs-demo/hbase-hdfs/hadoop
+kubectl create -f yaml/journalnode.yaml
+sleep 10s
+
+
+kubectl create -f yaml/namenode0.yaml
+kubectl create -f yaml/namenode1.yaml
+sleep 30s
+
+kubectl create -f yaml/datanode.yaml
+sleep 60s
+
+cd ~/imcs-demo/hbase-hdfs
+kubectl create -f hmaster.yaml
+kubectl create -f region.yaml
+
+
