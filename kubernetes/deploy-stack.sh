e14e67151ca4ed749c4696b5bfb8368856f498f7
pillaid <dinesh.pillai@ig.com>
2019-01-10 14:27:56 +0000

    MU-114 Deploy stack now contains deploying the hadoop cluster + other relevant changes

--- kubernetes/deploy-stack.sh
+++ kubernetes/deploy-stack.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-if [ -z "$1" ]
-  then
-    echo "External Public IP not passed in"
-    exit
-fi
 
-if [ -z "$2" ]
+if [ -z "$1" ]
   then
     echo "Build version number not provided"
     exit
 fi
 
-BUILDVERSION=$2
+BUILDVERSION=$1
 git checkout $BUILDVERSION
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 docker login -u dineshpillai -p Pill2017
 
-HOSTIPADDRESS=$1
-HBASECONTAINERID=hbasehost
+HOSTIPADDRESS=zoo1
+HBASECONTAINERID=hbase-master-a
 
 
 
@@ -43,16 +38,42 @@ docker push dineshpillai/imcs-tradequeryservice
 cd ../positionqueryservice
 mvn docker:build
 docker push dineshpillai/imcs-positionqueryservice
+cd ~/imcs-demo/database
+mvn docker:build
+docker push dineshpillai/innovation-mu-database-utility
+
+#Build hbase,hadoop-base, hadoop-journal, hadoop-namenode
+cd ~/imcs-demo/hbase-hdfs/hadoop
+make
+cd ~/imcs-demo/hbase-hdfs/hbase
+make
 
 #Connect up to Hbase to create the tables and schema
-echo "$HOSTIPADDRESS hbasehost" >> /etc/hosts
-cd ~/imcs-demo/database/target
-java -jar database-$BUILDVERSION.jar hbasehost=$HOSTIPADDRESS zkhost=$HOSTIPADDRESS
+#echo "$HOSTIPADDRESS hbasehost" >> /etc/hosts
+#cd ~/imcs-demo/database/target
+#java -jar database-$BUILDVERSION.jar hbasehost=$HOSTIPADDRESS zkhost=$HOSTIPADDRESS
 
 kubectl create namespace mu-architecture-demo
 kubectl create clusterrolebinding default-admin --clusterrole cluster-admin --serviceaccount=mu-architecture-demo:default
 
-#Deploy to Kubernetes
+#Deploy hbase, hadoop cluster
+cd ~/imcs-demo/hbase-hdfs/hadoop
+kubectl create -f yaml/journalnode.yaml
+sleep 10s
+
+
+kubectl create -f yaml/namenode0.yaml
+kubectl create -f yaml/namenode1.yaml
+sleep 30s
+
+kubectl create -f yaml/datanode.yaml
+sleep 60s
+
+cd ~/imcs-demo/hbase-hdfs
+kubectl create -f hmaster.yaml
+kubectl create -f region.yaml
+
+#Deploy the rest of the stack to Kubernetes
 cd ~/imcs-demo/kubernetes
 kubectl apply -f run-mzk.yaml
 hzformated=`cat "run-hz-jet-cluster.yaml" | sed -e "s/{{HOSTIPADDRESS}}/$HOSTIPADDRESS/g; s/{{HBASECONTAINERID}}/$HBASECONTAINERID/g"`
@@ -74,6 +95,9 @@ kubectl create -f manifests/trade-query-config.yml
 cd ~/imcs-demo/trade-injector
 kubectl apply -f manifests/trade-injector.yml
 kubectl apply -f manifests/trade-injector-configmap.yml
+
+cd ~/imcs-demo/database
+kubectl create -f yaml/database-connect.yaml
 
 cd ~/imcs-demo/kubernetes
 kubectl apply -f jobmanager-controller.yaml
